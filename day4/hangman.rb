e25614b6cd361e846fbddcedb11c59711b5b398e
Khin Maung Latt <khinlatt@Khins-MacBook-Air.local>
2014-07-31 11:58:17 -0400

    new hangman

--- day4/hangman.rb
+++ day4/hangman.rb
@@ -1,10 +1,61 @@
 class Hangman
-  def initialize
-
+  def initialize answer
+    @answer = answer
+    @guessed = []
+    @misses_remaining = 6
   end
 
+  def available_letters
+    ('a'..'z').to_a - @guessed ########## cool
+  end
+
+  def board
+    # Take @answer and only show letters that the player has guessed
+    result = ""
+    @answer.split("").each do |char|
+      if @guessed.include?(char)
+        result += char
+      else
+        result += "_"
+      end
+    end
+    result
+  end
+
+  # or attr_reader :misses_remaining
+  def misses_remaining
+    @misses_remaining
+  end
+
+  # TODO: think about this
+  def guess(letter)
+    found = @answer.include?(letter)
+
+    unless @guessed.include?(letter)
+      @guessed << letter
+      @misses_remaining -= 1 unless found
+    end
+    found
+  end
 
   def display
 
   end
+
+  def finished?
+    won? || lost?
+  end
+
+  def won?
+    !board.include?("_")
+  end
+
+  def lost?
+    @misses_remaining.zero?
+  end
 end
+
+
+#----------Runnning Script----------
+# puts "Let's play Hangman!"
+# game1 = Hangman.new
